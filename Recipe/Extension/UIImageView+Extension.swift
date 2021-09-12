2713965d9e98c5a5fe89316216f2b0d248f1d112
Nirajan Shrestha <nirajan.shrestha@ekbana.info>
2021-09-12 19:56:01 +0545

    image caching issue fixed

--- Recipe/Extension/UIImageView+Extension.swift
+++ Recipe/Extension/UIImageView+Extension.swift
@@ -12,10 +12,16 @@ extension UIImageView {
     
     func setImage(urlString: String?, placeHolderImage: UIImage? = UIImage(named: "placeholderImage"), completion: ((UIImage?)->())? = nil) {
         kf.indicatorType = .activity
+        
         if let urlString = urlString,
            let encodedURLString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
            let url = URL(string: encodedURLString) {
-            self.kf.setImage(with: url, placeholder: placeHolderImage) { (imageResult) in
+            
+            let processor = DownsamplingImageProcessor(size: self.bounds.size)
+            let options: KingfisherOptionsInfo = [
+                .processor(processor),
+            ]
+            self.kf.setImage(with: url, placeholder: placeHolderImage, options: options) { (imageResult) in
                 switch imageResult {
                 case .success(let imageResult):
                     completion?(imageResult.image)
@@ -23,6 +29,7 @@ extension UIImageView {
                     completion?(nil)
                 }
             }
+            
         } else {
             image = placeHolderImage
         }
